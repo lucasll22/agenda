80a49dd6019f160a73c8144fc14202333360784d
lucasll22 <lucaslusa2@gmail.com>
2018-03-29 14:14:09 -0300

    ajustes tela principal

--- app/src/main/java/com/example/lucas2/agenda/MainActivity.java
+++ app/src/main/java/com/example/lucas2/agenda/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.lucas2.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +15,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void prosseguir(View view) {
+        Intent intent = new Intent(this, MainScreen.class);
+        TextView nome = findViewById(R.id.nome);
+        TextView inst = findViewById(R.id.instituição);
 
+        intent.putExtra("nome", nome.getText().toString());
+        intent.putExtra("inst", inst.getText().toString());
+        startActivity(intent);
 
 
     }
--- app/src/main/java/com/example/lucas2/agenda/MainScreen.java
+++ app/src/main/java/com/example/lucas2/agenda/MainScreen.java
@@ -1,11 +1,9 @@
 package com.example.lucas2.agenda;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -13,17 +11,15 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Intent intent = getIntent();
+        String nome = intent.getExtras().getString("nome");
+        String inst = intent.getExtras().getString("inst");
+
+        TextView tela = findViewById(R.id.tela);
+
+        tela.setText("Bem vindo " + nome);
+        tela.append("\n" + inst);
+
     }
-
 }
