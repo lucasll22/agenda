90bd245ba53a4d11504e80b0374742a4a82ae618
lucasll22 <lucaslusa2@gmail.com>
2018-03-29 14:42:34 -0300

    Criadas as novas telas, ainda não concluídas

--- app/src/main/java/com/example/lucas2/agenda/MainScreen.java
+++ app/src/main/java/com/example/lucas2/agenda/MainScreen.java
@@ -3,7 +3,10 @@ package com.example.lucas2.agenda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -20,6 +23,29 @@ public class MainScreen extends AppCompatActivity {
 
         tela.setText("Bem vindo " + nome);
         tela.append("\n" + inst);
+
+    }
+
+    public void buttonMenu(View view) {
+        Log.w("tela principal", Integer.toString(view.getId()));
+        switch(view.getId())
+        {
+            case R.id.materias :
+                Toast.makeText(this, "materias", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, Materias.class);
+                startActivity(intent);
+                break;
+            case R.id.provas :
+                Toast.makeText(this, "provas", Toast.LENGTH_SHORT).show();
+                Intent intent2 = new Intent(this, Provas.class);
+                startActivity(intent2);
+                break;
+            case R.id.trabalhos :
+                Toast.makeText(this, "trabalhos", Toast.LENGTH_SHORT).show();
+                Intent intent3 = new Intent(this, Trabalhos.class);
+                startActivity(intent3);
+                break;
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/lucas2/agenda/Trabalhos.java
@@ -0,0 +1,13 @@
+package com.example.lucas2.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Trabalhos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trabalhos);
+    }
+}
