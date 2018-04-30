9dafc5c42423af8eb134654540e3f67bac1560b1
lucasll22 <lucaslusa2@gmail.com>
2018-04-30 18:15:59 -0300

    Comentário inicial

--- app/src/main/java/com/example/lucas2/agenda/MainActivity.java
+++ app/src/main/java/com/example/lucas2/agenda/MainActivity.java
@@ -1,3 +1,20 @@
+/*
+Estrutura das classes
+    MainActivity tem a tela inicial que pede uns dados
+    MainScreen tem a tela inicial do app, com o relatório dos proximos eventos e as opções para as demais telas
+    Materias, Provas e Trabalhos contém as tres telas para gerenciar e adicionar novos itens
+    NovaMateria, NovaProva, NovoTrabalho tem as telas para gerenciar a adição de dados no app
+    Pacote Principal tem as tres classes do modelo do app, já com seus getString para as listas
+
+ O que falta fazer
+    Ajustar layouts
+    Aquele esquema de criação do banco de dados NoSQL
+    Salvar e carregar do banco
+
+
+ */
+
+
 package com.example.lucas2.agenda;
 
 import android.content.Intent;
